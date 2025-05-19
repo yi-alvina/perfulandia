297d9153cf4a0894c33f3b88b41878f20b3bd27a
tomas <toma.zamora@duocuc.cl>
2025-05-19 11:45:09 -0400

    creacion de mircoservicios.

---
+++ msvc.carrito/src/test/java/com/tomzamora/msvc/carrito/msvc/carrito/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomzamora.msvc.carrito.msvc.carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ msvc.venta/src/main/java/com/tomzamora/msvc/venta/Application.java
@@ -0,0 +1,13 @@
+package com.tomzamora.msvc.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
