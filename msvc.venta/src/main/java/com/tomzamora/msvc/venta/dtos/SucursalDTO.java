972a6701067c499d9181cf949a192e46c7213b41
tomas <toma.zamora@duocuc.cl>
2025-05-26 23:02:11 -0400

    Ejecccion casi..

---
+++ msvc.venta/src/main/java/com/tomzamora/msvc/venta/dtos/SucursalDTO.java
@@ -0,0 +1,13 @@
+package com.tomzamora.msvc.venta.dtos;
+
+
+import lombok.*;
+
+@Getter @Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SucursalDTO {
+
+    private Long Sucursal;
+}
--- msvc.venta/src/main/java/com/tomzamora/msvc/venta/repositories/VentaReporitory.java
+++ msvc.venta/src/main/java/com/tomzamora/msvc/venta/repositories/VentaReporitory.java
@@ -1,5 +1,6 @@
 package com.tomzamora.msvc.venta.repositories;
 
+import com.tomzamora.msvc.venta.model.entities.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 
-public interface VentaReporitory extends JpaRepository<VentaReporitory, Long> {
+public interface VentaReporitory extends JpaRepository<Venta,Long> {
 
     List<VentaReporitory> findBy(Long idVenta);
 }
