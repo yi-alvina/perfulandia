bec92c4d8d4beda054bc1f6cf4c851186bad388e
MarcoCO99 <marc.contrerasa@duocuc.cl>
2025-06-23 13:32:22 -0400

    Add files via upload

---
+++ msvc-usuario/src/main/test/java/com/mcontreras/msvc/usuario/MsvcUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcontreras.msvc.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
