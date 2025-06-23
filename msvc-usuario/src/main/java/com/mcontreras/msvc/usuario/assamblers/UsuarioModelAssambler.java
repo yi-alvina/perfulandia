f0b37f916f7735c9de06d3e1b77e57c8ad0771d1
marco <marc.contrerasa@duocuc.cl>
2025-06-23 12:07:05 -0400

    agregar a la rama main lo agregado en la rama de pruebas unitarias

---
+++ msvc-usuario/src/main/java/com/mcontreras/msvc/usuario/assamblers/UsuarioModelAssambler.java
@@ -0,0 +1,26 @@
+package com.mcontreras.msvc.usuario.assamblers;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.stereotype.Component;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import com.mcontreras.msvc.sucursal.models.entities.Usuario;
+
+@Component
+public class UsuarioModelAssembler implements RepresentationModelAssembler<Usuario, EntityModel<Usuario>>{
+
+    @Override
+    public EntityModel<Usuario> toModel(Usuario entity) {
+
+        return EntityModel.of(
+                entity,
+                linkTo(methodOn(ProductoControllerV2.class).findById(entity.getProductoId())).withSelfRel(),
+                linkTo(methodOn(ProductoControllerV2.class).findAll()).withRel("productos")
+
+        );
+    }
+}
