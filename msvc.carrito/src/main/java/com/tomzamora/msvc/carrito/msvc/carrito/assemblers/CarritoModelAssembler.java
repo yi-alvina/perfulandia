b1bcc6c8f54c86f88b691e8500c11e1f5d95bf03
tomas <toma.zamora@duocuc.cl>
2025-06-23 23:57:48 -0400

    Integracion de packages(config, assembler y init) Actualizacion V2. Ultima actualizacion

--- msvc.carrito/src/main/java/com/tomzamora/msvc/carrito/msvc/carrito/assemblers/CarritoModelAssembler.java
+++ msvc.carrito/src/main/java/com/tomzamora/msvc/carrito/msvc/carrito/assemblers/CarritoModelAssembler.java
@@ -1,14 +1,30 @@
 package com.tomzamora.msvc.carrito.msvc.carrito.assemblers;
 
 
+import com.tomzamora.msvc.carrito.msvc.carrito.controller.CarritoControllerV2;
 import com.tomzamora.msvc.carrito.msvc.carrito.model.entities.Carrito;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
-public class CarritoModelAssembler implements RepresentationModelAssembler<Carrito, EntityModel<Carrito>> {}
+public class CarritoModelAssembler implements RepresentationModelAssembler<Carrito, EntityModel<Carrito>> {
 
     @Override
-    public Ent
+    public EntityModel<Carrito> toModel(Carrito entity) {
+        return EntityModel.of(
+                entity,
+                linkTo(methodOn(CarritoControllerV2.class).findById (entity.getIdVenta())).withSelfRel(),
+                linkTo(methodOn(CarritoControllerV2.class).findAll()).withRel("productos")
 
+        );
+    }
 }
+
+
+
+
+
