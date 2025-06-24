ebefa1b330739f0248b51afe21ed0eac44147bab
MarcoCO99 <marc.contrerasa@duocuc.cl>
2025-06-23 22:55:56 -0400

    correccion errores venta

--- msvc.venta/src/main/java/com/tomzamora/msvc/venta/assemblers/VentaModelAssembler.java
+++ msvc.venta/src/main/java/com/tomzamora/msvc/venta/assemblers/VentaModelAssembler.java
@@ -7,6 +7,9 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
 public class VentaModelAssembler implements RepresentationModelAssembler<Venta, EntityModel<Venta>> {
 
